039d2f64f28cc68f4062866f0c0a9d84dd726448
pcmbp <pichao314@gmail.com>
2020-05-10 20:48:12 -0700

    receive logic finished

--- receive.php
+++ receive.php
@@ -2,7 +2,7 @@
 // open Session
 //session_start();
 if (!isset($_POST['username']) or !isset($_POST['password'])) {
-    exit("Username or Password Not found");
+    exit("sername or password NOT FOUND");
 }
 ?>
 <!DOCTYPE HTML>
@@ -23,8 +23,44 @@ if (!isset($_POST['username']) or !isset($_POST['password'])) {
 
 $data = json_encode($_POST, JSON_PRETTY_PRINT);
 
-print_r($data)
+print_r($data);
 
+$name = $_POST['username'];
+$password = $_POST['password'];
+
+$servername = "127.0.0.1";
+$dbusername = "root";
+$dbpassword = "password";
+$dbname = "pc314";
+
+# accept form data
+$fname = explode('@', $name)[0];
+$lname = $fname;
+$email = $name;
+$addr = $fname . "s Home";
+$hphone = "88888888";
+$cphone = "88888888";
+
+
+$sql = "INSERT INTO Users (firstname, lastname, email, password,homeaddr, homephone, cellphone) VALUES ('"
+    . $fname . "','" . $lname . "','" . $email . "','" . $password . "','" . $addr . "','" . $hphone . "','" . $cphone .
+    "');";
+
+echo $sql . "<br>";
+
+// Create connection
+$conn = new mysqli($servername, $dbusername, $dbpassword, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} else {
+    echo "Database connected!<br>";
+    $result = $conn->query($sql);
+    echo $result . "<br>";
+    echo "User added!<br>";
+    $conn->close();
+}
 
 ?>
 
