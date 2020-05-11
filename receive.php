887725c232d7ee97a6c2c9ebb39f2691ff0b97ac
pcmbp <pichao314@gmail.com>
2020-05-10 17:58:16 -0700

    receive added

---
+++ receive.php
@@ -0,0 +1,29 @@
+<?php
+// open Session
+//session_start();
+?>
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>PC314 - Receive</title>
+</head>
+
+<body>
+<div>
+    <a href="index.php">
+        <img src="resource/logo.png" alt="LOGO" style="float: left" width="196" height="70"/>
+    </a><br><br><br><br>
+</div>
+
+<?php
+
+$data = json_encode($_POST, JSON_PRETTY_PRINT);
+
+print_r($data)
+
+?>
+
+</body>
+
+</html>
---
+++ send.php
@@ -0,0 +1,18 @@
+<?php
+//
+//
+//$url = "localhost/receive.php";
+//$data = array("username" => "First name", "last_name" => "last name", "email" => "email@gmail.com");
+//$postdata = json_encode($data);
+//
+//
+//try {
+//    $ch = curl_init($url);
+//    curl_setopt($ch, CURLOPT_POST, 1);
+//    curl_setopt($ch, CURLOPT_POSTFIELDS, $postdata);
+//    $result = curl_exec($ch);
+//    curl_close($ch);
+//}
+//catch(Exception $e){
+//    throw new Exception("Invalid URL",0,$e);
+//}
